12172c5726c887ba4bd6556fcbc8fdf8f3348244
Stijn Degruytere <uil.nestelen@gmail.com>
2026-09-13 18:13:25 +0200

    Introduced MP

--- Scripts/Combat/Combatants/Combatant.gd
+++ Scripts/Combat/Combatants/Combatant.gd
@@ -7,8 +7,32 @@ var grid_coordinate: Vector2i
 var max_health: int = 100
 var health: int = 100
 
+var max_movement_points: int = 3
+var movement_points: int = 3
+
+
 func set_grid_coordinate(new_coordinate: Vector2i) -> void:
 	grid_coordinate = new_coordinate
+
+
+func reset_movement_points() -> void:
+	movement_points = max_movement_points
+
+
+func spend_movement_points(amount: int) -> bool:
+	if amount < 0:
+		return false
+
+	if movement_points < amount:
+		return false
+
+	movement_points -= amount
+	return true
+
+
+func has_movement_points(amount: int) -> bool:
+	return movement_points >= amount
+
 
 func move_to(new_coordinate: Vector2i) -> bool:
 	var success := grid_manager.move_occupant(
