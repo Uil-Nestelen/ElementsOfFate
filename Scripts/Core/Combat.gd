6a403333e3addfe9595ab4da5d486a089ccf7a91
Stijn Degruytere <uil.nestelen@gmail.com>
2026-09-13 20:00:47 +0200

    I found the issue of the button not working I accidently removed the connection in the combat file. I tested it and it works again now

--- Scripts/Core/Combat.gd
+++ Scripts/Core/Combat.gd
@@ -6,6 +6,8 @@ func _ready() -> void:
 	battle_manager.initialize_battle()
 	battle_manager.start_battle()
 
+	$MapButton.pressed.connect(_on_map_button_pressed)
+
 func _on_map_button_pressed() -> void:
 	battle_manager.end_battle()
 	GameManager.return_to_map()
