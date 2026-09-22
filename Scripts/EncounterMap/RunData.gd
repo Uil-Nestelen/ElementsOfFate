411d2ac13aad7b19d94506dd00c536a5deae1d06
Uil-Nestelen <uil.nestelen@gmail.com>
2026-09-22 14:16:22 +0200

    Establish five-floor run seed generation

--- Scripts/EncounterMap/RunData.gd
+++ Scripts/EncounterMap/RunData.gd
@@ -8,3 +8,15 @@ var floors: Array[EncounterMap] = []
 
 func _init(run_seed: int) -> void:
 	master_seed = run_seed
+
+	var rng := RandomNumberGenerator.new()
+	rng.seed = master_seed
+
+	for floor_number in range(1, FLOOR_COUNT + 1):
+		var floor_seed := rng.randi()
+		var floor_map := EncounterGenerator.generate_floor(
+			floor_number,
+			floor_seed
+		)
+
+		floors.append(floor_map)
